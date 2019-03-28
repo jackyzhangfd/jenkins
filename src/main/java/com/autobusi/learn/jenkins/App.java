942b2269d2d5840205f655b56bdebf7f2f07b9be
I042102 <I042102@PVGN50917286A.apj.global.corp.sap>
2019-03-28 15:01:14 +0800

    add jenkinsfile

--- src/main/java/com/autobusi/learn/jenkins/App.java
+++ src/main/java/com/autobusi/learn/jenkins/App.java
@@ -6,8 +6,8 @@ package com.autobusi.learn.jenkins;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args){
+		for(int i=1;i<=10;i++)
+			System.out.println("Hello World " + i);
+	}
 }
